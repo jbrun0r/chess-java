6e3850fa1634708ae568c7544330b268f1c1e41d
João Rodrigues <joaobruno.rf@gmail.com>
2023-07-14 02:01:59 -0300

    feat: Placing pieces on board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetuo();
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -19,6 +23,11 @@ public class ChessMatch {
 		}
 		return mat;
 
+	}
+
+	private void initialSetuo() {
+		board.PlacePiece(new Rook(board, Color.BLACK), new Position(2, 1));
+		board.PlacePiece(new King(board, Color.BLACK), new Position(0, 4));
 	}
 
 }
