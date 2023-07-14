b2ce28cab15e55ef5b00ca36ed080ffc94e64594
João Rodrigues <joaobruno.rf@gmail.com>
2023-07-14 01:23:40 -0300

    feat: Chess layer

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,10 +1,10 @@
 package boardgame;
 
 public class Piece {
-	
+
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
 	}
@@ -12,6 +12,5 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
-	
+
 }
